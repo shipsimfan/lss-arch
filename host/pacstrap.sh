0f8b40abbb4b7b3ed00d94a4cf835d6a208f4ebc
Lance Hart <me@lancehart.ca>
2024-01-08 14:38:29 -0500

    Updated pacman invocation

--- host/pacstrap.sh
+++ host/pacstrap.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Make sure the keyring is updated
-pacman -Sy archlinux-keyring
+pacman -Sy archlinux-keyring --noconfirm
 
 # Install the packages
 pacstrap -K /mnt $(cat $1 | grep -v '^#')
