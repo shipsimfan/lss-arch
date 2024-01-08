910347c825ebb4f18c26cb8a9331426523e1f850
Lance Hart <me@lancehart.ca>
2024-01-08 11:43:59 -0500

    Fixed printing issue

--- install-host/partition.sh
+++ install-host/partition.sh
@@ -8,7 +8,7 @@ SWAP_SIZE=+"$2"G
 
 echo "Partitioning $1 . . ."
 
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${1} > /dev/null
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${1} &> /dev/null
     g # Create a new GUID partition table
     n # Create the boot partition
      # Default partition number 
